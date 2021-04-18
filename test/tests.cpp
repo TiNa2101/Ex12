670b072efa3d56cc81f0ed6ed2ad914c215dc8fc
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-04-19 01:48:51 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -32,7 +32,7 @@ TEST(set1, test3) {
     tDoor.unlock();
   }
   catch(std::string message) {
-    EXPECT_EQ(message, "Close the door!");
+    EXPECT_EQ(message, "Close the door please!");
   }
 }
 
